35743b8c5e92c7d472ad5f975ffd452db6d2f644
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2024-08-31 14:16:25 +0800

    Add files via upload

--- Modules/playtween.lua
+++ Modules/playtween.lua
@@ -3,28 +3,8 @@ Module.ClassName = "PlayTween"
 
 local TweenService = game:GetService("TweenService")
 
-function Module:PlayTween(Inst, Info, ...)
-    local Args = { ... }
-    local Destination = {}
-    local Meta = true
-    for _ = 1, #Args do
-        if Meta then
-            local New = Args[_ + 1]
-            if New then
-                Destination[Args[_]] = New
-            else
-                error("Missing argument in #" .. _ + 1)
-            end
-            Meta = false
-        else
-            Meta = true
-        end
-    end
-    local Tween = TweenService:Create(
-        Inst,
-        Info,
-        Destination
-    )
+function Module:PlayTween(...)
+    local Tween = TweenService:Create(...)
     Tween:Play()
     return Tween
 end
