6d56c50dc2896edbaaef1c1b3ec6650ffc4ea63b
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2025-02-01 15:12:25 +0800

    Add files via upload

--- loader.lua
+++ loader.lua
@@ -1,5 +1,1 @@
-if game.PlaceId == 142823291 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/374f04890e6b546798591472785cb607.lua"))()
-else
-    return error("Unsupported Game!")
-end
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/374f04890e6b546798591472785cb607.lua"))()
